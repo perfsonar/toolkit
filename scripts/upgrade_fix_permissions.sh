7a5100f079eb18b73c0da0f5f8b3e3b5812d5331
aaron <aaron@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2010-10-05 12:32:25 +0000

    Recursively set all of /var/lib/perfsonar to be owned by the perfsonar user on bootup (as part of the script to fix permissions on the live cd)
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/trunk@4503 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- scripts/upgrade_fix_permissions.sh
+++ scripts/upgrade_fix_permissions.sh
@@ -15,7 +15,7 @@ chown -R apache:perfsonar /var/log/perfsonar/web_admin
 
 # Make sure that the various /var/lib/perfsonar directories are correct.
 mkdir -p /var/lib/perfsonar
-chown perfsonar:perfsonar /var/lib/perfsonar
+chown -R perfsonar:perfsonar /var/lib/perfsonar
 
 mkdir -p /var/lib/perfsonar/snmp_ma
 chown -R perfsonar:perfsonar /var/lib/perfsonar/snmp_ma
