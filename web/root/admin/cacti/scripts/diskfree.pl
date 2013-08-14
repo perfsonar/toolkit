bf429ff488d916f3813a25a87e275bf6f9bce89b
alake <alake@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2013-08-14 19:40:23 +0000

    Upgrading cacti. Commit 1 of 3. Removing old cacti.
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/trunk@5704 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- web/root/admin/cacti/scripts/diskfree.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-open(PROCESS,"df --block-size=1024 -P $ARGV[0] | grep -v Filesystem |");
-foreach (<PROCESS>) {
-	if ($_ =~ /($ARGV[0])(.* )(.*[0-9])(.* )(.*[0-9])(.* )(.*[0-9])(.* )(.*[0-9])%(.* )/) {
-		print "megabytes:$7 percent:$9";
-	}
-}
-close(PROCESS);
--- web/root/admin/cacti/scripts/diskfree.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-df -k $1 | grep -v Filesystem| awk '{printf "megabytes:" $4 " percent:" int($5)}'
