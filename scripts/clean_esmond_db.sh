84ab13729aa35b9a12dc1ac142fe9b2a39cb3ecb
Andy Lake <andy@es.net>
2014-06-30 13:36:13 -0400

    Small fix for esmond db clean script. Not assuming its executable and calling with python

--- scripts/clean_esmond_db.sh
+++ scripts/clean_esmond_db.sh
@@ -3,4 +3,4 @@
 cd /opt/esmond
 source /opt/rh/python27/enable
 . bin/activate
-./util/ps_remove_data.py -c /opt/perfsonar_ps/toolkit/etc/clean_esmond_db.conf
+python ./util/ps_remove_data.py -c /opt/perfsonar_ps/toolkit/etc/clean_esmond_db.conf
