7c72123b3cb27ea5e4390721f191fe50d3ddb1c7
Andy Lake <andy@es.net>
2014-08-14 10:20:51 -0400

    Setting some reguired environment variables in clean_esmond_db.sh

--- scripts/clean_esmond_db.sh
+++ scripts/clean_esmond_db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+export ESMOND_ROOT=/opt/esmond
+export ESMOND_CONF=$ESMOND_ROOT/esmond.conf
+export DJANGO_SETTINGS_MODULE=esmond.settings
 cd /opt/esmond
 source /opt/rh/python27/enable
 . bin/activate
