718954970f563fcd371d194d122270648f84c74b
Michael Johnson <mj82@grnoc.iu.edu>
2015-10-16 20:53:13 +0000

    Add display of whether host is globally registered to Host Details section

--- web-ng/root/js/components/HostStatusSidebarComponent.js
+++ web-ng/root/js/components/HostStatusSidebarComponent.js
@@ -68,6 +68,13 @@ HostStatusSidebarComponent._setStatus = function( topic ) {
     } else {
         data.classes = 'color-red';
     }
+    
+    data.registered = (data.globally_registered == 1 ? "Yes" : "No");
+    if (data.globally_registered == 1 ) {
+        data.classes = 'color-green';
+    } else {
+        data.classes = 'color-red';
+    }
 
     var toolkit_version = data.toolkit_version;
     if (toolkit_version !== null) {  
