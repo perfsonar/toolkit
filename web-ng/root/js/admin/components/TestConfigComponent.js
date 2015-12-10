bb48f17e92a72a857cbd6c7c00911d16405e44b8
Michael Johnson <mj82@grnoc.iu.edu>
2015-12-10 19:56:52 +0000

    Begin building regular test listing table

--- web-ng/root/js/admin/components/TestConfigComponent.js
+++ web-ng/root/js/admin/components/TestConfigComponent.js
@@ -7,13 +7,51 @@ var TestConfigComponent = {
 TestConfigComponent.initialize = function() {
     //$('#loading-modal').foundation('reveal', 'open');
     Dispatcher.subscribe( TestConfigComponent.testConfigTopic, TestConfigComponent._showConfig );
+
+    // Hide subrows on load
+    $(".js-subrow").hide();
+
+    $(".js-row").click(function(){
+        $(".js-subrow[data-group="+$(this).attr("data-group")+"]").toggle();
+    });
+
+    // Add new host to test members
+    $(".js-add-new-host").click(function(e) {
+        e.preventDefault();
+        $("tbody.test-members").append('<tr class="subrow subrow--content"><td><input type="text"></td><td><input type="text"></td><td><label class="inline-label">IPv4</label> <input type="checkbox"></td><td><label class="inline-label">IPv6</label> <input type="checkbox"></td><td><a href="#"><i class="fa fa-trash"></i></a></td>');
+        $(".js-add-new-wrapper").hide();
+        $(".new-host-save").show();
+    });
+
+   
+};
+
+TestConfigComponent._showTable = function( data, tableView ) {
+    if ( tableView == undefined ) {
+        tableView = 'byHost';
+    }
+    console.log('tableView', tableView);
+
+    var test_template = $("#testConfigByHostTableTemplate").html();
+    var template = Handlebars.compile(test_template);
+    var test_table = template(data);
+    $("#testConfigContainer").html(test_table);
+
 };
 
 TestConfigComponent._showConfig = function() {
+    console.log('Test Config Topic received, showing config ...');
     console.log('testconfigcomponent TestConfigStore.getStatus()', TestConfigStore.getStatus() );
     console.log('testconfigcomponent TestConfigStore.getTestConfiguration()', TestConfigStore.getTestConfiguration() );
     console.log('testconfigcomponent TestConfigStore.getData()', TestConfigStore.getData() );
-    console.log('testconfigcomponent TestConfigStore', TestConfigStore );
+    console.log('testconfigcomponent TestConfigStore.getAllTestMembers()', TestConfigStore.getAllTestMembers() );
+
+    var data = TestConfigStore.getTestsByHost();
+    console.log('testconfigcomponent TestConfigStore.getTestsByHost()', data );
+
+    // ** Test config tables **
+    TestConfigComponent._showTable( data );
+
 };
 
 TestConfigComponent.initialize();
--- web-ng/root/js/stores/TestConfigStore.js
+++ web-ng/root/js/stores/TestConfigStore.js
@@ -5,7 +5,7 @@ var TestConfigStore = new DataStore("store.change.test_config", "services/regula
 //var TestConfigStore = new DataStore;
 //TestConfigStore.initialize("store.change.test_config", "services/regular_testing.cgi");
 
-console.log(TestConfigStore);
+console.log('TestConfigStore', TestConfigStore);
 
 Dispatcher.subscribe('store.change.test_config', function() {
     console.log('data from dispatcher/testconfigstore', TestConfigStore.getData()); 
@@ -16,5 +16,61 @@ TestConfigStore.getTestConfiguration = function() {
 };
 
 TestConfigStore.getStatus = function() {
+    console.log('getting status');
     return this.data.status;
 };
+
+
+TestConfigStore.getAllTestMembers = function() {
+    var member_array = [];
+    for(var i in this.data.test_configuration) {
+        var test = this.data.test_configuration[i];
+        for(var j in test.members) {
+            var member = test.members[j];
+            member_array.push(member.address);
+        }
+    }
+    return member_array;
+};
+
+
+TestConfigStore.getTestsByHost = function() {
+    var tests = {};
+    var member_array = [];
+    for(var i in this.data.test_configuration) {
+        var test = this.data.test_configuration[i];
+        for(var j in test.members) {
+            var member = test.members[j];
+            var address = member.address;
+            tests = TestConfigStore.addHostToTest(tests, test, member); 
+        }
+    }
+    var tests_sorted = [];
+    var keys = Object.keys(tests);
+    keys.sort();
+    for(var i in keys) {
+        tests_sorted.push(tests[ keys[i] ]);
+    }
+    return tests_sorted;
+};
+
+TestConfigStore.addHostToTest = function (tests, test, member) {
+    var address = member.address;
+    var type = test.type;
+    var type_count_name = type + "_count";
+    if ( !(address in tests) ) {
+        tests[address] = {};
+        tests[address].tests = [];
+    }
+
+    tests[address].tests.push(test);
+    tests[address].address = address;
+    if ( test[type_count_name] ) {
+        tests[address][type_count_name]++;
+    } else {
+        tests[address][type_count_name] = 1;
+    }
+    return tests;
+
+};
+
