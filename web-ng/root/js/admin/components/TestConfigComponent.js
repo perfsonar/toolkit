a6b5e4a5a562240c84b1fca5b4a1084d1aa0475b
Michael Johnson <mj82@grnoc.iu.edu>
2016-02-09 17:45:45 +0000

    Add test deletion to GUI

--- web-ng/root/js/admin/components/TestConfigComponent.js
+++ web-ng/root/js/admin/components/TestConfigComponent.js
@@ -349,6 +349,21 @@ TestConfigComponent.showTestConfigModal = function( testID ) {
     return false;
 };
 
+TestConfigComponent.deleteTest = function( testID ) {
+    //var data = TestConfigStore.data;
+    var testConfig = TestConfigStore.getTestConfig( testID );
+    var description = testConfig.description;
+    var choice = window.confirm('Do you really want to delete the test "' + testConfig.description + '"? Your changes will not take effect until you click the Save button.');
+    if ( choice ) {
+        var success = TestConfigStore.deleteTest( testID );
+        if ( success ) {
+            Dispatcher.publish( TestConfigStore.topic );
+            SharedUIFunctions._showSaveBar();
+        }
+    }
+    return false;
+};
+
 TestConfigComponent._drawConfigForm = function( testConfig ) {
     if ( testConfig.type ) {
         testConfig.defaults = $.extend( true, {}, TestConfigStore.data.defaults.type[ testConfig.type ] );
