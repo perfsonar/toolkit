267bef4a22cdac6a5ab7144905d0ea9a73ce1425
Valentin Vidic <Valentin.Vidic@CARNet.hr>
2016-05-31 13:35:40 +0200

    Update package names after rename
    (cherry picked from commit 364d4b1fdbe76316fabd41f1e22a5feb895649a9)

--- scripts/install-optional-packages.py
+++ scripts/install-optional-packages.py
@@ -16,7 +16,12 @@ import yum
 import Internet2Lib
 import Internet2Consts
 
-optionalPackages=['perl-perfSONAR_PS-Toolkit-ntp','perl-perfSONAR_PS-Toolkit-security','perl-perfSONAR_PS-Toolkit-service-watcher','perl-perfSONAR_PS-Toolkit-sysctl']
+optionalPackages=[
+    'perfsonar-toolkit-ntp',
+    'perfsonar-toolkit-security',
+    'perfsonar-toolkit-servicewatcher',
+    'perfsonar-toolkit-sysctl'
+]
 
 def installPackages(yumHandle, packageNames):
     userChoice=[]
